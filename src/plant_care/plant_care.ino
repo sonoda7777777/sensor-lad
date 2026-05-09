fab74f56fee13042f6b69a00bd7fc2c39556c9e0
unknown <sonoda2468@gmail.com>
2026-05-09 21:43:48 +0900

    fix: plant_careのリファクタリング

--- src/plant_care/plant_care.ino
+++ src/plant_care/plant_care.ino
@@ -1,56 +1,53 @@
 #include <ChainableLED.h>
 
-#define NUM_LEDS 1
+constexpr uint8_t NUM_LEDS            = 1;
+constexpr uint8_t LED_CLOCK_PIN       = 7;
+constexpr uint8_t LED_DATA_PIN        = 8;
+constexpr uint8_t MOISTURE_SENSOR_PIN = A0;
+constexpr uint8_t LED_INDEX           = NUM_LEDS - 1;
 
-const int moistureSensorPin = A0; // 水分センサの値を読み取るピン
+constexpr uint16_t DRY_THRESHOLD          = 400;
+constexpr uint16_t WET_NOTIFY_DURATION_MS = 3000;
+constexpr uint16_t LOOP_INTERVAL_MS       = 1000;
+constexpr uint32_t SERIAL_BAUD_RATE       = 9600;
 
-bool isDry = true; // 土が乾燥しているかを判断する変数
-
-// cin_pin, data_pin, LEDの数
-ChainableLED leds(7, 8, NUM_LEDS);
-
-struct color {
-  byte red = 0;
-  byte green = 0;
-  byte blue = 0;
+struct Color {
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
 };
 
+constexpr Color COLOR_OFF  = {0,   0,   0};
+constexpr Color COLOR_RED  = {255, 0,   0};
+constexpr Color COLOR_BLUE = {0,   0,   255};
+
+ChainableLED leds(LED_CLOCK_PIN, LED_DATA_PIN, NUM_LEDS);
+
+void setLedColor(const Color& color) {
+  leds.setColorRGB(LED_INDEX, color.red, color.green, color.blue);
+}
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(SERIAL_BAUD_RATE);
 }
 
 void loop() {
-  // 水分センサから値を読み取る
-  int moistureSensorValue = analogRead(moistureSensorPin);
+  static bool isDry = true;
 
-  const int dryThreshold = 600; // 乾燥を表す閾値
+  const uint16_t moistureValue = analogRead(MOISTURE_SENSOR_PIN);
 
-  const byte led = NUM_LEDS - 1; // LEDを識別する番号
-
-  color led_color = {0, 0, 0};
-
-  // 土に水が入った場合、ユーザが分かるようにするために3秒間青色を発光
-  if(dryThreshold <= moistureSensorValue && isDry) {
-    led_color.red   = 0;
-    led_color.green = 0;
-    led_color.blue  = 255;
-    leds.setColorRGB(led, led_color.red, led_color.green, led_color.blue);
-    delay(3000);
-    isDry = false;
-  }
-  // 土が湿っている場合、LEDは消灯にしておく
-  else if(dryThreshold <= moistureSensorValue && !isDry) {
-    leds.setColorRGB(led, led_color.red, led_color.green, led_color.blue);
-  }
-  // 土が乾燥している場合、赤色を発光
-  if(dryThreshold > moistureSensorValue) {
-    led_color.red   = 255;
-    led_color.green = 0;
-    led_color.blue  = 0;
-    leds.setColorRGB(led, led_color.red, led_color.green, led_color.blue);
+  if (moistureValue < DRY_THRESHOLD) {
+    setLedColor(COLOR_RED);
     isDry = true;
+  } else if (isDry) {
+    // 乾燥→湿潤への遷移をユーザに知らせるため3秒間青色を発光
+    setLedColor(COLOR_BLUE);
+    delay(WET_NOTIFY_DURATION_MS);
+    isDry = false;
+  } else {
+    setLedColor(COLOR_OFF);
   }
-  
-  delay(1000);
+
+  delay(LOOP_INTERVAL_MS);
 }
 
